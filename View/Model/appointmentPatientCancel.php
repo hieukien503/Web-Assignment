8951f6a37911dd5f17aa50f05ec312e4128fb986
Đoàn Hồng Hiếu Kiên <92379724+hieukien503@users.noreply.github.com>
2023-12-02 11:23:19 +0700

    Add files via upload

---
+++ View/Model/appointmentPatientCancel.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+function deletingAppointment($date,$timeslot)
+{ 
+    include("connectDB.php");
+    $sql = "UPDATE appointment SET appointment_status='I' WHERE appointment_timeslot='$timeslot' AND appointment_date = '$date'";
+    $result = $conn->query($sql);
+    $conn->close();
+    return $result;
+}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['appointdate']) && isset($_POST['timeslotpatient'])) {
+        if (deletingAppointment($_POST['appointdate'], $_POST['timeslotpatient'])) {
+            $_SESSION['delete'] = true;
+            header("Location: ../index.php");
+            exit();
+        } else {
+            echo "<script>window.alert(Invalid username or password)</script>";
+            echo "notgood";
+        }
+    } else {
+        echo "<script>Please provide both username and password</script>";
+        header("Location: ../login.php");
+    }
+}
+?>
