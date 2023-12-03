f1156253c75ce172d42eb11996b98b230a49d2a1
Khoa LTN <vietnamkhoale@gmail.com>
2023-12-03 11:17:25 +0700

    fix: initDB

--- Model/initDB.php
+++ Model/initDB.php
@@ -3,10 +3,11 @@ class InitDatabase
 {
     private $servername = "localhost";
     private $username = "root";
-    private $password = "1234";
+    private $password = "";
     private $dbname = "BOOKING_APPOINTMENT";
 
     public $conn = null;
+    private $lock_init = false;
 
     public function __construct()
     {
@@ -14,11 +15,21 @@ class InitDatabase
          * The very first connection to initialize the database schema.
          * Query content is sourced from DB.SQL
          */
-        $db_schema_query = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/dashboard/Web-Assignment-main/Model/DB.SQL");
+        $db_schema_query = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/Model/DB.SQL");
 
         $init_conn = new mysqli($this->servername, $this->username, $this->password);
+
+        $duplicate_db = $init_conn->query("SHOW DATABASES LIKE '$this->dbname'");
+        if ($duplicate_db->num_rows > 0) {
+            // If the database is already existed, the initialization should stop
+            $this->lock_init = true;
+            $init_conn->close();
+            return;
+        }
+
         $init_conn->multi_query($db_schema_query);
         $init_conn->close();
+        sleep(1);
 
         /**
          * After database initialized, connect to the database.
@@ -28,18 +39,20 @@ class InitDatabase
 
     public function addAccount($fullname, $mail, $password, $role)
     {
+        if ($this->lock_init == true)  return;
+
         // Check if account exists or not;
         if ($this->conn->query("SELECT * FROM users WHERE email = '$mail'")->num_rows > 0)
             return;
-        if ($this->conn->query("SELECT * FROM users WHERE email = '$mail'")->num_rows > 0)
-            return;
 
-        $sql = "INSERT INTO users (fullName, email, password,role) VALUES ('$fullname', '$mail', '$password','$role')";
+        $sql = "INSERT INTO Users (fullName, email, password,role) VALUES ('$fullname', '$mail', '$password','$role')";
         $result = $this->conn->query($sql);
     }
 
     public function closeConnection()
     {
+        if ($this->lock_init == true)  return;
+
         $this->conn->close();
     }
 }
@@ -48,17 +61,11 @@ class InitDatabase
 $init_db = new InitDatabase();
 
 // Add a few doctors
-$init_db->addAccount('Dr. Kien', 'kien.le123@hcmut.edu.vn', 'byebye', 1);
-$init_db->addAccount('Dr. Jackson', 'jack@gmail.com', '123456', 1);
-$init_db->addAccount('Dr. Steve', 'khoa.lesteve@hcmut.edu.com', '123456', 1);
 $init_db->addAccount('Dr. Kien', 'kien.le123@hcmut.edu.vn', 'byebyebyebyebye', 1);
 $init_db->addAccount('Dr. Jackson', 'jack@gmail.com', '12345678', 1);
 $init_db->addAccount('Dr. Steve', 'khoa.lesteve@hcmut.edu.com', '12345678', 1);
 
 // Add a few patients
-$init_db->addAccount('Duong Van Hao', 'dvhao@gmail.vn', '123456', 0);
-$init_db->addAccount('Lai Van Minh', 'minh@gmail.com', '123456', 0);
-$init_db->addAccount('Lam Phuong', 'phuong@saigonair.com', '123456', 0);
 $init_db->addAccount('Duong Van Hao', 'dvhao@gmail.com', '12345678', 0);
 $init_db->addAccount('Lai Van Minh', 'minh@gmail.com', '12345678', 0);
 $init_db->addAccount('Lam Phuong', 'phuong@saigonair.com', '12345678', 0);
